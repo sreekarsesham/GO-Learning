4ca3179e3e522d5378818ff90b50f3c002b54381
sreekarsesham <sreekarspersonal@gmail.com>
2021-10-03 10:42:53 +0530

    read csv

---
+++ ReadCSV/main.go
@@ -0,0 +1,17 @@
+skjbsdhbqj
+
+
+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
+
+ git config --global user.name "sreekarsesham"
+ git config --global user.email "sreekarspersonal@gmail.com"
+ git config -l
+ 
+ 
+ git clone https://github.com/sreekarsesham/GO-Learning.git
+
+ Username for 'https://github.com' : username
+ Password for 'https://github.com' : give your personal access token here
+ 
+ 
+ git clone https://<tokenhere>@github.com/<user>/<repo>.git
